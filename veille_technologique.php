40d1fffe5fe84b32e64d84ccb92d91ca347bb474
Brewen06 <brewen68@ik.me>
2025-04-24 12:25:24 +0200

    Update veille_technologique.php

--- veille_technologique.php
+++ veille_technologique.php
@@ -1,9 +1,10 @@
-<html>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-<title>Veille Technologique</title>
+	<title>Veille Technologique</title>
 	<meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 <header>
